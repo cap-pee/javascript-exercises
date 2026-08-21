3eddbe01a6fadf85b5cd2b68cab80eba104d5a54
cap-pee <abdurrafeyamir15@gmail.com>
2026-08-21 19:24:46 +0500

    Exercise 07 done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newstring = '';
+    for (let i = -1; i >= -string.length; i--) {
+        newstring += string.at(i)
+    }
+    return newstring
 };
 
 // Do not edit below this line
