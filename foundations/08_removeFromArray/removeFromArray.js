827cb9591414f07b92269331e5ad7063d80e5833
cap-pee <abdurrafeyamir15@gmail.com>
2026-08-21 19:38:18 +0500

    Exercise 08 done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    let newArray = array.filter((num) => !args.includes(num));
+    return newArray
 };
 
 // Do not edit below this line
