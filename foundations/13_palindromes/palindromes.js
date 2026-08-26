fcace437f531ca5447847dd8780a4ffd30d7f5d5
cap-pee <abdurrafeyamir15@gmail.com>
2026-08-26 17:36:58 +0500

    Exercise 13 done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const valid = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+                                .toLowerCase()
+                                .split('')
+                                .filter(char => valid.includes(char))
+                                .join('')
+    
+    const reversed = cleanedString
+                                .split('')
+                                .reverse()
+                                .join('')
+    
+    return cleanedString === reversed;
 };
 
+
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;
