eaa0fbdd08af2f52fe4a53395ed18d8f4bf88820
cap-pee <abdurrafeyamir15@gmail.com>
2026-08-26 17:52:29 +0500

    Exercise 14 done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) return 'OOPS'
+    if (num == 0) return 0
+
+    let prev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= num; i++) {
+        let current = secondPrev + prev;
+        secondPrev = prev;
+        prev = current;
+    }
+
+    return prev
 };
 
 // Do not edit below this line
