259c44ebb5449b3dff68e6e0833d6650d84eb9fe
cap-pee <abdurrafeyamir15@gmail.com>
2026-08-27 22:38:07 +0500

    Exercise 16 done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let arr2 = array.sort((p1, p2) => {
+
+        const currentYear = new Date().getFullYear();
+        let p1Age, p2Age
+
+        if (p1.yearOfDeath === undefined) {
+            p1.yearOfDeath = currentYear;
+        }
+        if (p2.yearOfDeath === undefined) {
+            p2.yearOfDeath = currentYear;
+        }
+
+        p1Age = p1.yearOfDeath - p1.yearOfBirth
+        p2Age = p2.yearOfDeath - p2.yearOfBirth
+
+        return (p2Age - p1Age)
+    })
+
+    return arr2[0]
 
 };
 
